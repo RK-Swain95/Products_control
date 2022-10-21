be4883cc7a543778178addc36a1d1c27e832ef75
Radhakanta Swain <tata.swain@gmail.com>
2022-10-21 20:53:19 +0530

    adding port to env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const app= express();
-const port=8000;
+const port=process.env.PORT || 8000;
 const dotenv=require("dotenv").config();
 //const rateLimit = require("express-rate-limit");
 const expresslayouts=require('express-ejs-layouts');
